4a5816543ceec325ca1b4fe8deb0d4cec6696d81
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-01-26 19:38:19 -0500

    feat: `build_cfg` library and tool

---
+++ lesson2/build-cfg/src/lib.rs
@@ -0,0 +1,281 @@
+// Copyright (C) 2024 Ethan Uppal.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::{collections::HashMap, mem};
+
+use bril_rs::{Code, EffectOps, Function, Instruction, Position};
+use slotmap::{new_key_type, Key, SecondaryMap, SlotMap};
+use snafu::{whatever, OptionExt, Whatever};
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Label {
+    pub name: String,
+}
+
+new_key_type! { pub struct BasicBlockIdx; }
+
+impl BasicBlockIdx {
+    pub fn as_number(&self) -> u64 {
+        // hacky, but it doesn't really matter here
+        self.data().as_ffi()
+    }
+}
+
+#[derive(Default)]
+pub struct BasicBlock {
+    pub is_entry: bool,
+    pub label: Option<Label>,
+    pub instructions: Vec<Instruction>,
+    pub exit: Option<LabeledExit>,
+}
+
+pub enum LabeledExit {
+    Unconditional {
+        label: String,
+        pos: Option<Position>,
+    },
+    Conditional {
+        condition: String,
+        if_true_label: String,
+        if_false_label: String,
+        pos: Option<Position>,
+    },
+    Return(Option<String>),
+}
+
+#[derive(Clone)]
+pub enum Exit {
+    Unconditional(BasicBlockIdx),
+    Conditional {
+        condition: String,
+        if_true: BasicBlockIdx,
+        if_false: BasicBlockIdx,
+    },
+    Return(Option<String>),
+}
+
+#[derive(Default)]
+pub struct ControlFlowGraph {
+    pub vertices: SlotMap<BasicBlockIdx, BasicBlock>,
+    pub edges: SecondaryMap<BasicBlockIdx, Exit>,
+}
+
+#[derive(Default)]
+struct ControlFlowGraphBuilder {
+    cfg: ControlFlowGraph,
+    current_block: BasicBlock,
+    labels_to_blocks: HashMap<String, BasicBlockIdx>,
+}
+
+impl ControlFlowGraphBuilder {
+    pub fn current_block_is_empty(&self) -> bool {
+        self.current_block.instructions.is_empty()
+    }
+
+    pub fn add_to_current(&mut self, instruction: Instruction) {
+        self.current_block.instructions.push(instruction);
+    }
+
+    pub fn set_current_label(&mut self, name: String) {
+        self.current_block.label = Some(Label { name });
+    }
+
+    pub fn set_current_exit(&mut self, exit: LabeledExit) {
+        self.current_block.exit = Some(exit);
+    }
+
+    pub fn mark_current_as_entry(&mut self) {
+        self.current_block.is_entry = true;
+    }
+
+    pub fn finish_current_and_start_new_block(&mut self) {
+        let current_label = self.current_block.label.clone();
+        let current_block = mem::take(&mut self.current_block);
+        let block_idx = self.cfg.vertices.insert(current_block);
+        if let Some(label) = current_label {
+            self.labels_to_blocks.insert(label.name.clone(), block_idx);
+        }
+    }
+
+    pub fn finish(mut self) -> Result<ControlFlowGraph, Whatever> {
+        for (block_idx, block) in &self.cfg.vertices {
+            if let Some(exit) = &block.exit {
+                match exit {
+                    LabeledExit::Unconditional { label: always, pos } => {
+                        let destination_index = *self
+                            .labels_to_blocks
+                            .get(always)
+                            .whatever_context(format!(
+                                "Unknown label {} referenced at {}",
+                                always,
+                                pos_to_string(pos.as_ref())
+                            ))?;
+                        self.cfg.edges.insert(
+                            block_idx,
+                            Exit::Unconditional(destination_index),
+                        );
+                    }
+                    LabeledExit::Conditional {
+                        condition,
+                        if_true_label,
+                        if_false_label,
+                        pos,
+                    } => {
+                        let if_true_index = *self
+                            .labels_to_blocks
+                            .get(if_true_label)
+                            .whatever_context(format!(
+                                "Unknown label {} referenced at {}",
+                                if_true_label,
+                                pos_to_string(pos.as_ref())
+                            ))?;
+                        let if_false_index = *self
+                            .labels_to_blocks
+                            .get(if_false_label)
+                            .whatever_context(format!(
+                                "Unknown label {} referenced at {}",
+                                if_false_label,
+                                pos_to_string(pos.as_ref())
+                            ))?;
+                        self.cfg.edges.insert(
+                            block_idx,
+                            Exit::Conditional {
+                                condition: condition.clone(),
+                                if_true: if_true_index,
+                                if_false: if_false_index,
+                            },
+                        );
+                    }
+                    LabeledExit::Return(value) => {
+                        self.cfg
+                            .edges
+                            .insert(block_idx, Exit::Return(value.clone()));
+                    }
+                }
+            }
+        }
+
+        Ok(self.cfg)
+    }
+}
+
+fn pos_to_string(pos: Option<&Position>) -> String {
+    pos.map(|pos| format!("{}:{}", pos.pos.row, pos.pos.col))
+        .unwrap_or("<unknown>".into())
+}
+
+pub fn build_cfg(function: &Function) -> Result<ControlFlowGraph, Whatever> {
+    let mut builder = ControlFlowGraphBuilder::default();
+
+    builder.mark_current_as_entry();
+
+    for instruction in &function.instrs {
+        match instruction {
+            Code::Label { label, .. } => {
+                if !builder.current_block_is_empty() {
+                    builder.finish_current_and_start_new_block();
+                }
+                builder.set_current_label(label.clone());
+            }
+            Code::Instruction(instruction) => match instruction {
+                Instruction::Effect {
+                    labels,
+                    op: EffectOps::Jump,
+                    pos,
+                    ..
+                } => {
+                    let [destination_label] = labels.as_slice() else {
+                        whatever!(
+                            "Jump operation at {} should take one label",
+                            pos_to_string(pos.as_ref())
+                        );
+                    };
+
+                    builder.set_current_exit(LabeledExit::Unconditional {
+                        label: destination_label.clone(),
+                        pos: pos.clone(),
+                    });
+
+                    builder.finish_current_and_start_new_block();
+                }
+                Instruction::Effect {
+                    args,
+                    labels,
+                    op: EffectOps::Branch,
+                    pos,
+                    ..
+                } => {
+                    let [condition] = args.as_slice() else {
+                        whatever!(
+                            "Branch operation at {} should take one condition argument",
+                            pos_to_string(pos.as_ref())
+                        );
+                    };
+                    let [if_true_label, if_false_label] = labels.as_slice()
+                    else {
+                        whatever!(
+                            "Branch operation at {} should take two labels",
+                            pos_to_string(pos.as_ref())
+                        );
+                    };
+
+                    builder.set_current_exit(LabeledExit::Conditional {
+                        condition: condition.clone(),
+                        if_true_label: if_true_label.clone(),
+                        if_false_label: if_false_label.clone(),
+                        pos: pos.clone(),
+                    });
+
+                    builder.finish_current_and_start_new_block();
+                }
+                Instruction::Effect {
+                    args,
+                    labels,
+                    op: EffectOps::Return,
+                    pos,
+                    ..
+                } => {
+                    let value = match args.as_slice() {
+                        [] => None,
+                        [value] => Some(value.clone()),
+                        _ => whatever!(
+                            "Branch operation at {} should take one condition argument",
+                            pos_to_string(pos.as_ref())
+                        )
+                    };
+                    if !labels.is_empty() {
+                        whatever!(
+                            "Return operation at {} should take no labels",
+                            pos_to_string(pos.as_ref())
+                        );
+                    };
+
+                    builder.set_current_exit(LabeledExit::Return(value));
+
+                    builder.finish_current_and_start_new_block();
+                }
+                other => {
+                    builder.add_to_current(other.clone());
+                }
+            },
+        }
+    }
+
+    // for anything leftover
+    if !builder.current_block_is_empty() {
+        builder.finish_current_and_start_new_block();
+    }
+
+    builder.finish()
+}
---
+++ lesson2/build-cfg/src/main.rs
@@ -0,0 +1,193 @@
+// Copyright (C) 2024 Ethan Uppal.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::{fs, io, io::Write, path::PathBuf};
+
+use argh::FromArgs;
+use bril_rs::Program;
+use build_cfg::{build_cfg, Exit};
+use inform::{common::IndentWriterCommon, io::IndentWriter};
+use owo_colors::OwoColorize;
+use snafu::{ResultExt, Whatever};
+
+/// Extracts the control-flow graph from a Bril program.
+#[derive(FromArgs)]
+struct Opts {
+    /// input Bril file: omit for stdin
+    #[argh(positional)]
+    input: Option<PathBuf>,
+}
+
+#[snafu::report]
+fn main() -> Result<(), Whatever> {
+    let opts = argh::from_env::<Opts>();
+
+    let program: Program = if let Some(path) = opts.input {
+        let contents = fs::read_to_string(&path).whatever_context(format!(
+            "Failed to read the contents of {}",
+            path.to_string_lossy()
+        ))?;
+        serde_json::from_str(&contents).whatever_context(
+            "Failed to parse input file as a valid Bril program",
+        )?
+    } else {
+        serde_json::from_reader(io::stdin()).whatever_context(
+            "Failed to parse standard input as a valid Bril program",
+        )?
+    };
+
+    let mut stdout = io::stdout();
+    let mut f = IndentWriter::new(&mut stdout, 4);
+
+    for function in &program.functions {
+        let cfg = build_cfg(function).whatever_context(format!(
+            "Failed to build control-flow graph for function `{}`",
+            function.name
+        ))?;
+
+        writeln!(f, "{} {{", function.name.bold().white())
+            .whatever_context("Writing to stdout failed")?;
+        f.increase_indent();
+
+        for (i, (block_idx, block)) in cfg.vertices.iter().enumerate() {
+            if i > 0 {
+                writeln!(f).whatever_context("Writing to stdout failed")?;
+            }
+
+            if block.is_entry {
+                write!(f, "{} -> ", "<entry>".bold().bright_green())
+                    .whatever_context("Writing to stdout failed")?;
+            }
+
+            if let Some(label) = &block.label {
+                write!(f, ".{} ", label.name.on_truecolor(64, 64, 64))
+                    .whatever_context("Writing to stdout failed")?;
+            }
+            writeln!(
+                f,
+                "[{}] {{",
+                block_idx.as_number().to_string().bold().bright_green()
+            )
+            .whatever_context("Writing to stdout failed")?;
+
+            f.increase_indent();
+            for instruction in &block.instructions {
+                writeln!(
+                    f,
+                    "{}",
+                    instruction.to_string().truecolor(128, 128, 128)
+                )
+                .whatever_context("Writing to stdout failed")?;
+            }
+            f.decrease_indent();
+            write!(f, "}} ").whatever_context("Writing to stdout failed")?;
+
+            if let Some(exit) = cfg.edges.get(block_idx).cloned() {
+                match exit {
+                    Exit::Unconditional(destination) => {
+                        write!(
+                            f,
+                            "-> {}",
+                            destination
+                                .as_number()
+                                .to_string()
+                                .bold()
+                                .bright_green()
+                        )
+                        .whatever_context("Writing to stdout failed")?;
+                        if let Some(label) = &cfg.vertices[destination].label {
+                            write!(
+                                f,
+                                " (.{})",
+                                label.name.on_truecolor(64, 64, 64)
+                            )
+                            .whatever_context("Writing to stdout failed")?;
+                        }
+                        writeln!(f)
+                            .whatever_context("Writing to stdout failed")?;
+                    }
+                    Exit::Conditional {
+                        condition,
+                        if_true,
+                        if_false,
+                    } => {
+                        write!(
+                            f,
+                            "({}) -> {}",
+                            condition.truecolor(128, 128, 128),
+                            if_true
+                                .as_number()
+                                .to_string()
+                                .bold()
+                                .bright_green()
+                        )
+                        .whatever_context("Writing to stdout failed")?;
+                        if let Some(label) = &cfg.vertices[if_true].label {
+                            write!(
+                                f,
+                                " (.{})",
+                                label.name.on_truecolor(64, 64, 64)
+                            )
+                            .whatever_context("Writing to stdout failed")?;
+                        }
+                        writeln!(f)
+                            .whatever_context("Writing to stdout failed")?;
+                        write!(
+                            f,
+                            "  ({}) -> {}",
+                            format!("!{}", condition).truecolor(128, 128, 128),
+                            if_false
+                                .as_number()
+                                .to_string()
+                                .bold()
+                                .bright_green()
+                        )
+                        .whatever_context("Writing to stdout failed")?;
+                        if let Some(label) = &cfg.vertices[if_false].label {
+                            write!(
+                                f,
+                                " (.{})",
+                                label.name.on_truecolor(64, 64, 64)
+                            )
+                            .whatever_context("Writing to stdout failed")?;
+                        }
+                        writeln!(f)
+                            .whatever_context("Writing to stdout failed")?;
+                    }
+                    Exit::Return(value) => {
+                        writeln!(
+                            f,
+                            "-> {}{}",
+                            "<return>".bold().bright_green(),
+                            if let Some(value) = value {
+                                format!(" {}", value)
+                            } else {
+                                "".into()
+                            }
+                            .truecolor(128, 128, 128)
+                        )
+                        .whatever_context("Writing to stdout failed")?;
+                    }
+                }
+            } else {
+                writeln!(f).whatever_context("Writing to stdout failed")?;
+            }
+        }
+
+        f.decrease_indent();
+        writeln!(f, "}}\n").whatever_context("Writing to stdout failed")?;
+    }
+
+    Ok(())
+}
