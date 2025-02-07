a62a19f2a3584df6e9a499e726225912e86a3b27
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-02-06 23:53:26 -0500

    feat(lvn): Basic constant folding prototype

--- lesson3/lvn/src/main.rs
+++ lesson3/lvn/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, fs, hash::Hash, io, path::PathBuf};
 
 use argh::FromArgs;
-use bril_rs::{Instruction, Program, Type, ValueOps};
+use bril_rs::{ConstOps, Instruction, Literal, Program, Type, ValueOps};
 use build_cfg::{print::print_cfg_as_bril_text, BasicBlock};
 use snafu::{ResultExt, Whatever};
 
@@ -49,12 +49,15 @@ struct ValueTable {
     intern: HashMap<Value, usize>,
     counter: usize,
     variables_to_values: HashMap<String, usize>,
+
+    constant_folder: HashMap<usize, Literal>,
 }
 
 impl ValueTable {
     fn add_value_and_get_existing_variable(
         &mut self,
         value: Value,
+        constant: Option<Literal>,
         current_variable: &str,
         is_overwritten: bool,
     ) -> (String, Option<String>) {
@@ -77,6 +80,10 @@ impl ValueTable {
             let new_value_index = self.values.len() - 1;
             self.intern.insert(value, new_value_index);
 
+            if let Some(constant) = constant {
+                self.constant_folder.insert(new_value_index, constant);
+            }
+
             self.variables_to_values
                 .insert(current_variable.to_owned(), new_value_index);
             (new_name, None)
@@ -91,6 +98,13 @@ impl ValueTable {
         match value {
             OpArg::Value(value) => self.values[value].1.clone(),
             OpArg::Unknown(other) => other,
+        }
+    }
+
+    fn get_constant(&self, value: OpArg) -> Option<&Literal> {
+        match value {
+            OpArg::Value(value) => self.constant_folder.get(&value),
+            OpArg::Unknown(_) => None,
         }
     }
 }
@@ -125,6 +139,7 @@ pub fn lvn(block: &mut BasicBlock) {
                     } else {
                         Value::OtherConst(value.to_string())
                     },
+                    Some(value.clone()),
                     dest,
                     is_overwritten,
                 ) {
@@ -171,6 +186,7 @@ pub fn lvn(block: &mut BasicBlock) {
                     .collect::<Vec<_>>();
                 match table.add_value_and_get_existing_variable(
                     Value::LeftAlone(NeverEqual),
+                    None,
                     dest,
                     is_overwritten,
                 ) {
@@ -213,6 +229,7 @@ pub fn lvn(block: &mut BasicBlock) {
                     .collect::<Vec<_>>();
                 match table.add_value_and_get_existing_variable(
                     Value::LeftAlone(NeverEqual),
+                    None,
                     dest,
                     is_overwritten,
                 ) {
@@ -270,6 +287,7 @@ pub fn lvn(block: &mut BasicBlock) {
                 }
                 match table.add_value_and_get_existing_variable(
                     Value::Op(*op, new_args.clone()),
+                    None,
                     dest,
                     is_overwritten,
                 ) {
@@ -284,18 +302,62 @@ pub fn lvn(block: &mut BasicBlock) {
                             op_type: op_type.clone(),
                         }
                     }
-                    (destination, None) => Instruction::Value {
-                        args: new_args
-                            .into_iter()
-                            .map(|value| table.get_canonical_name(value))
-                            .collect(),
-                        dest: destination,
-                        funcs: funcs.clone(),
-                        labels: labels.clone(),
-                        op: *op,
-                        pos: pos.clone(),
-                        op_type: op_type.clone(),
-                    },
+                    (destination, None) => {
+                        let constant_folded = if let Ok(constant_fold_attempt) =
+                            new_args
+                                .iter()
+                                .map(|arg| {
+                                    table
+                                        .get_constant(arg.clone())
+                                        .cloned()
+                                        .ok_or(())
+                                })
+                                .collect::<Result<Vec<Literal>, ()>>()
+                        {
+                            {
+                                match op {
+                                    ValueOps::Add => {
+                                        if let Ok(ints) = constant_fold_attempt
+                                            .into_iter()
+                                            .map(|literal| match literal {
+                                                Literal::Int(int) => Ok(int),
+                                                _ => Err(()),
+                                            })
+                                            .collect::<Result<Vec<i64>, ()>>()
+                                        {
+                                            Some(Instruction::Constant {
+                                                dest: destination.clone(),
+                                                op: ConstOps::Const,
+                                                pos: pos.clone(),
+                                                const_type: op_type.clone(),
+                                                value: Literal::Int(
+                                                    ints.into_iter().sum(),
+                                                ),
+                                            })
+                                        } else {
+                                            None
+                                        }
+                                    }
+                                    _ => None,
+                                }
+                            }
+                        } else {
+                            None
+                        };
+
+                        constant_folded.unwrap_or(Instruction::Value {
+                            args: new_args
+                                .into_iter()
+                                .map(|value| table.get_canonical_name(value))
+                                .collect(),
+                            dest: destination,
+                            funcs: funcs.clone(),
+                            labels: labels.clone(),
+                            op: *op,
+                            pos: pos.clone(),
+                            op_type: op_type.clone(),
+                        })
+                    }
                 }
             }
             Instruction::Effect {
