1a9b845629c9c661310487822d60ed7f7b6f36a8
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-03-01 06:51:23 -0500

    feat(ssa): Initial template for SSA program

---
+++ lesson6/ssa/src/main.rs
@@ -0,0 +1,39 @@
+use std::{fs, io, path::PathBuf};
+
+use argh::FromArgs;
+use bril_rs::Program;
+use snafu::{ResultExt, Whatever};
+
+/// Transforms Bril into and out of SSA
+#[derive(FromArgs)]
+struct Opts {
+    /// input Bril file: omit for stdin
+    #[argh(positional)]
+    input: Option<PathBuf>,
+}
+
+#[snafu::report]
+fn main() -> Result<(), Whatever> {
+    let opts = argh::from_env::<Opts>();
+
+    let program: Program = if let Some(path) = opts.input {
+        let contents = fs::read_to_string(&path).whatever_context(format!(
+            "Failed to read the contents of {}",
+            path.to_string_lossy()
+        ))?;
+        serde_json::from_str(&contents).whatever_context(
+            "Failed to parse input file as a valid Bril program",
+        )?
+    } else {
+        serde_json::from_reader(io::stdin()).whatever_context(
+            "Failed to parse standard input as a valid Bril program",
+        )?
+    };
+
+    for function in program.functions {
+        let cfg = build_cfg::build_cfg(&function, true)
+            .whatever_context("Failed to build cfg")?;
+    }
+
+    Ok(())
+}
